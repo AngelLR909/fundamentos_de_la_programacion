7bffa649c988a29de52164dd2b2ad2bcac1b0e82
AngelLR909 <aggelzx@gmail.com>
2026-08-17 10:30:23 -0600

    Limpieza rama semana 2

--- nombre.py
+++
@@ -1,4 +0,0 @@
-nombre = input("\nHola, ingresa tu nombre: ")
-edad = input("Ingresa tambien tu edad por favor: ")
-
-print(f"\nHola {nombre}, tienes {edad}, cualquier edad es ideal para empezar\n")
